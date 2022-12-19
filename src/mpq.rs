8334654b48e9902e6b13fb18a892918415c2423e
zzlk <2418897+zzlk@users.noreply.github.com>
2022-12-19 10:44:51 -0800

    clean up mpq.rs a bit

--- src/mpq.rs
+++ src/mpq.rs
@@ -1,71 +1,87 @@
+use anyhow::anyhow;
+use anyhow::bail;
+use anyhow::Error;
 use scopeguard::defer;
-use tracing::instrument;
+use std::ffi::CString;
+use std::io::Write;
+use std::path::Path;
+use stormlib_bindings::SFileCloseArchive;
+use stormlib_bindings::SFileCloseFile;
+use stormlib_bindings::SFileGetFileSize;
+use stormlib_bindings::SFileOpenFileEx;
+use stormlib_bindings::SFileReadFile;
+use stormlib_bindings::SFileSetLocale;
+use stormlib_bindings::ERROR_HANDLE_EOF;
+use stormlib_bindings::SFILE_INVALID_SIZE;
+use stormlib_bindings::{GetLastError, SFileOpenArchive, HANDLE};
+use tracing::{error, instrument};
+use uuid::Uuid;
 
-#[instrument(level = "trace")]
-pub fn get_chk_from_mpq_filename(filename: String) -> anyhow::Result<Vec<u8>, anyhow::Error> {
-    // This is really not the rust way to do things but stormlib_bindings is internally not threadsafe so what we can do.
+#[instrument(level = "trace", skip_all)]
+pub fn get_chk_from_mpq_filename<T: AsRef<Path>>(
+    filename: T,
+) -> anyhow::Result<Vec<u8>, anyhow::Error> {
     lazy_static::lazy_static! {
+        // This is really not the rust way to do things but stormlib_bindings is internally not threadsafe so what we can do.
         static ref LOCK: std::sync::Mutex<()> = std::sync::Mutex::new(());
     }
 
-    let cstr = std::ffi::CString::new(filename.as_str())?;
+    let cstr = CString::new(
+        filename
+            .as_ref()
+            .to_str()
+            .ok_or(anyhow!("Could not convert filename to str"))?,
+    )?;
 
     let _lock = LOCK.lock().unwrap();
     unsafe {
-        //let zzz = std::ffi::CString::new(filename.as_str())?;
-        let mut mpq_handle = 0 as stormlib_bindings::HANDLE;
-        if stormlib_bindings::SFileOpenArchive(cstr.as_ptr(), 0, 0, &mut mpq_handle as *mut _)
-            == false
-        {
-            return Err(anyhow::anyhow!(
-                "SFileOpenArchive. GetLastError: {}, filename: {filename}",
-                stormlib_bindings::GetLastError()
-            ));
+        let mut mpq_handle = 0 as HANDLE;
+        if SFileOpenArchive(cstr.as_ptr(), 0, 0, &mut mpq_handle as *mut _) == false {
+            bail!(
+                "SFileOpenArchive. GetLastError: {}, filename: {}",
+                GetLastError(),
+                filename.as_ref().to_string_lossy()
+            );
         }
 
         defer! {
-            if stormlib_bindings::SFileCloseArchive(mpq_handle) == false {
-                println!(
+            if SFileCloseArchive(mpq_handle) == false {
+                error!(
                     "{:?}",
-                    anyhow::anyhow!(
-                        "SFileCloseArchive. GetLastError: {}, filename: {filename}",
-                        stormlib_bindings::GetLastError()
+                    anyhow!(
+                        "SFileCloseArchive. GetLastError: {}, filename: {}",
+                        GetLastError(),
+                        filename.as_ref().to_string_lossy()
                     )
                 );
             }
         };
 
         let try_map_with_locale = |filename: &str, locale| {
-            //println!("try_map_with_locale: {filename}, locale: {locale}");
+            let cstr = CString::new(filename)?;
 
-            let cstr = std::ffi::CString::new(filename)?;
-
-            stormlib_bindings::SFileSetLocale(locale); // Set locale, this function never fails.
-            let mut archive_file_handle = 0 as stormlib_bindings::HANDLE;
-            if stormlib_bindings::SFileOpenFileEx(
+            SFileSetLocale(locale);
+            let mut archive_file_handle = 0 as HANDLE;
+            if SFileOpenFileEx(
                 mpq_handle,
                 cstr.as_ptr(),
                 0,
                 &mut archive_file_handle as *mut _,
             ) == false
             {
-                println!(
+                bail!(
                     "SFileOpenFileEx. GetLastError: {}, filename: {filename}, locale: {locale}",
-                    stormlib_bindings::GetLastError()
+                    GetLastError()
                 );
-                return Err(anyhow::anyhow!(
-                    "SFileOpenFileEx. GetLastError: {}, filename: {filename}, locale: {locale}",
-                    stormlib_bindings::GetLastError()
-                ));
             }
 
             defer! {
-                if stormlib_bindings::SFileCloseFile(archive_file_handle) == false {
-                    println!(
+                if SFileCloseFile(archive_file_handle) == false {
+                    error!(
                         "{:?}",
-                        anyhow::anyhow!(
+                        anyhow!(
                             "SFileCloseFile. GetLastError: {}, filename: {filename}, locale: {locale}",
-                            stormlib_bindings::GetLastError()
+                            GetLastError()
                         )
                     );
                 }
@@ -74,28 +90,25 @@ pub fn get_chk_from_mpq_filename(filename: String) -> anyhow::Result<Vec<u8>, an
             let file_size_low;
             let mut file_size_high: u32 = 0;
 
-            file_size_low = stormlib_bindings::SFileGetFileSize(
-                archive_file_handle,
-                &mut file_size_high as *mut _,
-            );
+            file_size_low = SFileGetFileSize(archive_file_handle, &mut file_size_high as *mut _);
 
-            if file_size_low == stormlib_bindings::SFILE_INVALID_SIZE {
-                return Err(anyhow::anyhow!(
+            if file_size_low == SFILE_INVALID_SIZE {
+                bail!(
                     "SFileGetFileSize. GetLastError: {}, filename: {filename}, locale: {locale}",
-                    stormlib_bindings::GetLastError()
-                ));
+                    GetLastError()
+                );
             }
 
             if file_size_high != 0 {
-                return Err(anyhow::anyhow!(
+                bail!(
                     "SFileGetFileSize. File size too big. file_size_high: {file_size_high}, file_size_low: {file_size_low}",
-                ));
+                );
             }
 
             let mut chk_data: Vec<u8> = vec![0; file_size_low as usize];
 
             let mut size: u32 = 0;
-            if stormlib_bindings::SFileReadFile(
+            if SFileReadFile(
                 archive_file_handle,
                 chk_data.as_mut_ptr() as *mut _,
                 chk_data.len() as u32,
@@ -103,19 +116,12 @@ pub fn get_chk_from_mpq_filename(filename: String) -> anyhow::Result<Vec<u8>, an
                 0 as *mut _,
             ) == false
             {
-                let last_error = stormlib_bindings::GetLastError();
-                if last_error != stormlib_bindings::ERROR_HANDLE_EOF
-                    || size == chk_data.len() as u32
-                {
-                    println!(
+                let last_error = GetLastError();
+                if last_error != ERROR_HANDLE_EOF || size == chk_data.len() as u32 {
+                    bail!(
                         "SFileReadFile. GetLastError: {}, filename: {filename}, locale: {locale}",
                         last_error,
                     );
-
-                    return Err(anyhow::anyhow!(
-                        "SFileReadFile. GetLastError: {}, filename: {filename}, locale: {locale}",
-                        last_error,
-                    ));
                 }
             }
 
@@ -132,124 +138,33 @@ pub fn get_chk_from_mpq_filename(filename: String) -> anyhow::Result<Vec<u8>, an
         // PROTECTION: Some maps put fake scenario.chk files at different locales. Try to find the real one by trying a lot of them.
         for locale in locales {
             if let Ok(x) = try_map_with_locale("staredit\\scenario.chk", locale) {
-                // println!("got candidate");
-                // PROTECTION: Some maps put very small scenario.chk files in the mpq at different locales, ignore them.
-                //if verify_is_valid_chk(x.as_slice()) {
                 return Ok(x);
-                //}
             }
         }
 
-        return Err(anyhow::anyhow!(
+        bail!(
             "Couldn't find scenario.chk the legit way: {}, file: {}",
-            stormlib_bindings::GetLastError(),
-            filename
-        ));
-
-        // If none of the files found are useful then fall back to enumarating every single file in the MPQ and check to see if it is a CHK file.;
-        // Until I'm confident i won't need this again, I'll just leave it here for now.
-        // {
-        //     let mut find_data: stormlib_bindings::SFILE_FIND_DATA =
-        //         stormlib_bindings::SFILE_FIND_DATA {
-        //             cFileName: [0i8; 1024],
-        //             szPlainName: 0 as *mut i8,
-        //             dwHashIndex: 0,
-        //             dwBlockIndex: 0,
-        //             dwFileSize: 0,
-        //             dwFileFlags: 0,
-        //             dwCompSize: 0,
-        //             dwFileTimeLo: 0,
-        //             dwFileTimeHi: 0,
-        //             lcLocale: 0,
-        //         };
-
-        //     let search_handle = stormlib_bindings::SFileFindFirstFile(
-        //         mpq_handle,
-        //         format!("{}\0", "*").as_ptr() as *const i8,
-        //         &mut find_data as *mut stormlib_bindings::SFILE_FIND_DATA,
-        //         0 as *mut _,
-        //     );
-
-        //     defer! {
-        //         if stormlib_bindings::SFileFindClose(search_handle) == false {
-        //             println!(
-        //                 "{:?}",
-        //                 anyhow::anyhow!(
-        //                     "SFileFindClose. GetLastError: {}, filename: {filename}",
-        //                     stormlib_bindings::GetLastError()
-        //                 )
-        //             );
-        //         }
-        //     }
-
-        //     loop {
-        //         if search_handle == 0 as stormlib_bindings::HANDLE {
-        //             return Err(anyhow::anyhow!(
-        //                 "search_handle == 0. No more files in MPQ. GetLastError: {}",
-        //                 stormlib_bindings::GetLastError()
-        //             ));
-        //         }
-
-        //         let new_filename =
-        //             crate::util::parse_null_terminated_string(&find_data.cFileName[0..]);
-
-        //         println!(
-        //             "Enumarating Files: cFileName: {}, dwBlockIndex: {}, dwCompSize: {}, dwFileSize: {}, dwFileTimeHi: {}, dwFileTimeLo: {}, dwHashIndex: {}, lcLocale: {}",
-        //             new_filename,
-        //             find_data.dwBlockIndex,
-        //             find_data.dwCompSize,
-        //             find_data.dwFileSize,
-        //             find_data.dwFileTimeHi,
-        //             find_data.dwFileTimeLo,
-        //             find_data.dwHashIndex,
-        //             find_data.lcLocale,
-        //         );
-
-        //         if let Ok(x) = try_map_with_locale(new_filename, find_data.lcLocale) {
-        //             // PROTECTION: Some maps put very small scenario.chk files in the mpq at different locales, ignore them.
-        //             if verify_is_valid_chk(x.as_slice()) {
-        //                 return Ok(x);
-        //             }
-        //         }
-
-        //         if stormlib_bindings::SFileFindNextFile(
-        //             search_handle,
-        //             &mut find_data as *mut stormlib_bindings::SFILE_FIND_DATA,
-        //         ) == false
-        //         {
-        //             return Err(anyhow::anyhow!(
-        //                 "SFileFindNextFile. No more files in MPQ. GetLastError: {}",
-        //                 stormlib_bindings::GetLastError()
-        //             ));
-        //         }
-        //     }
-        // }
+            GetLastError(),
+            filename.as_ref().to_string_lossy(),
+        );
     }
 }
 
 #[instrument(level = "trace", skip_all)]
-pub fn get_chk_from_mpq_in_memory(mpq: &[u8]) -> anyhow::Result<Vec<u8>, anyhow::Error> {
-    use std::io::Write;
+pub fn get_chk_from_mpq_in_memory(mpq: &[u8]) -> Result<Vec<u8>, Error> {
+    let path = format!("/tmp/{}.scx", Uuid::new_v4().as_simple().to_string());
 
-    let path = {
-        use std::str::FromStr;
-        let uuid = uuid::Uuid::new_v4().as_simple().to_string();
-        let path = std::path::PathBuf::from_str(format!("/tmp/{}.scx", uuid).as_str())?;
-
-        let mut file = std::fs::File::create(&path)?;
-
-        file.write(mpq)?;
-
-        file.flush()?;
-
-        path
-    };
+    let mut file = std::fs::File::create(&path)?;
 
     defer! {
-        if let Err(e) = std::fs::remove_file(&path) {
-            println!("{:?}", e);
+        if let Err(err) = std::fs::remove_file(&path) {
+            error!("{:?}", err);
         }
     }
 
-    get_chk_from_mpq_filename(path.to_string_lossy().to_string())
+    file.write(mpq)?;
+
+    file.flush()?;
+
+    get_chk_from_mpq_filename(&path)
 }
